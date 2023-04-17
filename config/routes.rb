acbd0e8b47ffa032b2af00dd234303aa31dcbabb
Spencer Vernon <svernon0@chicagobooth.edu>
2023-04-17 16:24:34 +0000

    First edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", {:controller => "currencies", :action => "homepage" })
+
+  get("/forex", { :controller => "currencies", :action => "first_currency" })
+
+  get("/forex/:first_symbol/:second_symbol"), { :controller => "currencies", :action => "second_currency" }
+
 end
